eb095dc64b1d8ac6393f013edc8bd429dab31732
Gary Alberto Cordero Rosa <garycordero1690@gmail.com>
2020-01-19 21:39:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   result =[]
   array = string.split(" ")
-  array.each{ |chr| chr = chr.reverse();  chr  }
+  array.each{ |chr| chr = chr.reverse();  result <<chr  }
 
 end
 
